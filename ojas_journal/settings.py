76f3774571d5de5c87be83d3fb344d24344fe9bc
srustith8 <srustith8@gmail.com>
2021-06-01 10:20:20 +0530

    updated code

--- ojas_journal/settings.py
+++ ojas_journal/settings.py
@@ -177,7 +177,5 @@ CKEDITOR_IMAGE_BACKEND = "pillow"
 CKEDITOR_UPLOAD_SLUGIFY_FILENAME = False
 
 
-# prod_db  =  dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
 
 django_heroku.settings(locals())
